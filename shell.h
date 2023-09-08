9ab49e6b9d7567241ab2b26e2b6c3616a1d720e6
Joeeazy <joeeazy34@gmail.com>
2023-09-08 18:31:38 +0300

    added a main header file

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/type.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+
+#endif
