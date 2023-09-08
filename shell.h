dba280d990ffe867cc661394733d58c622dcd97d
johnalexsagwe <johnsagwe@gmail.com>
2023-09-08 11:32:51 -0700

    shell.h update

--- shell.h
+++ shell.h
@@ -8,6 +8,16 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+/*  Maximum command length */
+#define MAX_COMMAND_LENGTH 1024
 
+/* function to display shell prompt */
+void display_prompt();
+
+/* function to read user input and return dynamically allocated string */
+char* read_input();
+
+/* function to process and execute a command based on user input */
+void execute_command(char* command);
 
 #endif
